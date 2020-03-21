9e5987d6deb68b83df71234ef2107f83d728b03d
Rihards Abo <denkfrt@inbox.lv>
2020-03-21 20:07:52 +0200

    Demo run enviroment init

---
+++ ConsoleTester/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using OldInterpreter;
+using RihaInterpreterLibrary;
+
+namespace ConsoleTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var compiler = new RihaCompiler();
+            var lines = File.ReadAllLines(@"..\..\..\DemoCode.txt");
+
+            //var code = string.Join('\n', lines);
+            Compiler.Run(lines);
+            for(var i =0; i < Output.OutputLines.Count; i++)
+                Console.WriteLine($" {i:D2} |  {Output.OutputLines[i]}");
+
+            //  compiler.Execute(lines);
+            //var output = compiler.CodeOutput.Split('\n');
+            //foreach (var line in output)
+            //{
+            //    Console.WriteLine(line);
+            //}
+        }
+    }
+}
