cb2b58b671a768e07a9c97f497f324e1b826c910
Rihards Abo <denkfrt@inbox.lv>
2020-03-22 15:30:41 +0200

    Rewritten prefix suffix logic for symbol translator

--- RihaInterpreterLibrary/Translator/SymbolTranslator.cs
+++ RihaInterpreterLibrary/Translator/SymbolTranslator.cs
@@ -31,31 +31,12 @@ namespace RihaInterpreterLibrary.Translator
                 {
                     // Gets value without quotes
                     var value = m.ToString().Replace(" ", "");
-                    var parts = value.Split(entire.Value.splitter);
 
-                    // TODO: rewrite
-                    var output = "";
-                    if (parts[0][0] == '\n')
-                    {
-                        output += "\n";
-                        parts[0] = parts[0].Replace("\n", "");
-                    }
+                    var prefix = value[0] == '\n' ? "\n" : "";
+                    var suffix = value[^1] == '\n' ? "\n" : "";
 
-                    output += $"{entire.Value.replacer}:{parts[0]}:";
-
-                    if (parts[1][^1] == '\n')
-                    {
-                        parts[1] = parts[1].Replace("\n", "");
-                        output += parts[1];
-                        output += "\n";
-                    }
-                    else
-                    {
-                        output += parts[1];
-                    }
-                    // -----
-
-                    return output;
+                    var parts = value.Replace("\n", "").Split(entire.Value.splitter);
+                    return $"{prefix}{entire.Value.replacer}:{parts[0]}:{parts[1]}{suffix}";
                 });
             }
             return code;
