e78d31a322a3adbe8267971f3a7f7e642a26cd5b
Rihards Abo <denkfrt@inbox.lv>
2020-09-16 18:07:10 +0300

    [FEATURE] New action: type_of

---
+++ RihaInterpreterLibrary/Actions/TypeOfAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RihaInterpreterLibrary.Actions
+{
+    public class TypeOfAction : IAction
+    {
+        public int ArgumentCount { get; } = 1;
+        public string ActionName { get; } = "type_of";
+
+        private const string ValidationPattern = @"type_of";
+
+        public bool IsValid(string action) => action.ToLower() == ValidationPattern;
+        public Node Execute(string[] actionInParts, List<Node> variables)
+        {
+            return new Node(variables[^1].Type.ToString(), ValueType.Text);
+        }
+    }
+}
